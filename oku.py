00ce9490f6993e5b54cd2012902146117062a0cc
KaiLawliet03 <54603993+KaiLawliet03@users.noreply.github.com>
2019-09-06 13:33:46 -0400

    Add files via upload

---
+++ oku.py
@@ -0,0 +1,12 @@
+
+print ("\x1b[32;1m Merhaba, ben\x1b[0m  \x1b[31;1m Kai. \x1b[0m")
+print ("\x1b[32;1m \x1b[1m")
+print ("Python dilinde kod yazmayı deniyorum ve kod dosyasını githuba atıp telefondan çalıştırmayı deniyorum.")
+print ("Eğer bunu okuyan herkimsen sana da göstermişimdir. Özelsin reis.")
+print ("Ya da ben söylemediğim halde bunu okuyabiliyorsan benim stalkerimsin.")
+
+
+print ("Zaman geçtikçe daha fazla öğreneceğim.")
+print ("Yararlı programlar yazacağım sizlere.")
+print ("")
+print ("Yine aklımda uçuk kaçık şeyler var ehe")
